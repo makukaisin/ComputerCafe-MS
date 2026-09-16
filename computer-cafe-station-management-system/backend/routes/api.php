95d6de48906bdd8c14e79b8785fd5d961d75f0f3
makukaisin <entredichomarkfrancis@gmail.com>
2026-09-16 08:20:17 +0800

    api php for the api of stationController acess-- Third Commit

---
+++ computer-cafe-station-management-system/backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\StationController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/stations', [StationController::class, 'index']);
+
+Route::post('/stations', [StationController::class, 'store']);
+
+Route::get('/stations/{station}', [
+    StationController::class,
+    'show'
+]);
